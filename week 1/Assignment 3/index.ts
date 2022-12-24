4bce6634f607301e7750632489d611900e8ad36f
oniseg <anasabdo704@gmail.com>
2022-12-24 20:07:17 +0300

    Assignment 3 week 1

--- week 1/Assignment 3/index.ts
+++ week 1/Assignment 3/index.ts
@@ -1,5 +1,6 @@
 function printInfo(valueOne:(string|number), valueTwo:(string|number)) {
   return `Value One Is ${valueOne}, Value Two Is ${valueTwo}`;
+  
 }
 
 console.log(printInfo(10, 20)); // Value One Is 10, Value Two Is 20
