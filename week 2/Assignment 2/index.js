ded2542a29082db7c0e6c5d46271f3c3347c3364
oniseg <anasabdo704@gmail.com>
2022-12-26 02:16:49 +0300

    Assignment 2 week 2

---
+++ week 2/Assignment 2/index.js
@@ -0,0 +1,6 @@
+(function () {
+    // Do Not Edit Here
+    var myInfo;
+    myInfo = 1000; // No Problem Here
+    myInfo = true; // No Problem Here
+})();
---
+++ week 2/Assignment 2/index.ts
@@ -0,0 +1,8 @@
+(function () {
+    // Write Your Code Here
+    type mix = number|boolean
+    // Do Not Edit Here
+    let myInfo: mix;
+    myInfo = 1000; // No Problem Here
+    myInfo = true; // No Problem Here
+})()
