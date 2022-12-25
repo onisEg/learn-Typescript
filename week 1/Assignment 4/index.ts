d3854eb39e75f492ddc43b748dc7f7515672140e
oniseg <anasabdo704@gmail.com>
2022-12-25 16:30:19 +0300

    self invoced function assignment 4

--- week 1/Assignment 4/index.ts
+++ week 1/Assignment 4/index.ts
@@ -1,3 +1,6 @@
+(function () {
+    let arr: (number | boolean[] | (string | (string | number)[])[])[];
+    arr = [2, [true, false], ["string", ["string", 2]]];
+    console.log(arr);
+    })()
 
-let arr: (number | boolean[] | (string | (string | number)[])[])[];
-arr = [2, [true, false], ["string", ["string", 2]]];
